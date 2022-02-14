5c250f2eb6c6d5e85af5d452d4c3acba3297b36f
Gerson <gedacaba@gmail.com>
2022-02-13 21:50:36 -0500

    agregando rutas y notas

--- app.js
+++ app.js
@@ -5,6 +5,26 @@ import path from 'path';
 
 const app = express();
 
+//conexión a bases de datos 
+//const mongoose = require('mongoose');
+import mongoose from 'mongoose';
+
+
+/* const uri = 'mongodb://localhost:27017/udemy';
+const options = {useNewUrlParser: true, useCreateIndex: true};
+// Or using promises
+mongoose.connect(uri, options).then(
+  () => { console.log('conectado a mongodb')},
+  err => { err }
+); */
+
+try {
+  mongoose.connect('mongodb://localhost:27017/udemy');
+  console.log('conectado a mongodb')
+} catch (error) {
+  handleError(error);
+}
+
 // Middleware
 app.use(morgan('tiny'));
 app.use(cors());
@@ -13,10 +33,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Rutas
-
+/* 
 app.get('/', (req, res) => {
   res.send('Hello World!');
-});
+}); */
+
+app.use('/api', require('./routes/nota'));
 
 
 // Middleware para Vue.js router modo history
@@ -26,5 +48,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('puerto', process.env.PORT || 3000);
 app.listen(app.get('puerto'), () => {
-  console.log('Example app listening on port'+ app.get('puerto'));
+  console.log('Escuchando por el puerto '+ app.get('puerto'));
 });
